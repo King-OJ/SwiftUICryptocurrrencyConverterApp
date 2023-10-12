6c9dc641c04669e3fafd004960ec2da56d44fcf3
Clement Ojiguo <clementojigs@hotmail.com>
2023-10-12 16:33:45 +0100

    Initial Commit

---
+++ CryptocurrrencyConverterApp/CryptocurrrencyConverterAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptocurrrencyConverterAppApp.swift
+//  CryptocurrrencyConverterApp
+//
+//  Created by Clem OJ on 12/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct CryptocurrrencyConverterAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
